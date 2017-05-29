ed9c3e99ded23134b1b3149babea5b95919d2eed
Arvind <europa1613@users.noreply.github.com>
2017-05-29 01:36:17 -0400

    Before and After

--- junit-mockito-ud/test/com/examples/junit/GreetingImplTest.java
+++ junit-mockito-ud/test/com/examples/junit/GreetingImplTest.java
@@ -2,13 +2,23 @@ package com.examples.junit;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GreetingImplTest {
 
+	private IGreeting greeting;
+
+	@Before
+	public void setup() {
+		System.out.println("setup");
+		greeting = new GreetingImpl();
+	}
+
 	@Test
 	public void greetShouldReturnAValidOutput() {
-		IGreeting greeting = new GreetingImpl();
+		System.out.println("greetShouldReturnAValidOutput");
 		String result = greeting.greet("JUnit");
 		assertNotNull(result);
 		assertEquals("Hello, JUnit", result);
@@ -16,14 +26,20 @@ public class GreetingImplTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void greetShouldThrowAnException_for_NameIsNull() {
-		IGreeting greeting = new GreetingImpl();
+		System.out.println("greetShouldThrowAnException_for_NameIsNull");
 		greeting.greet(null);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void greetShouldThrowAnException_for_NameIsBlank() {
-		IGreeting greeting = new GreetingImpl();
+		System.out.println("greetShouldThrowAnException_for_NameIsBlank");
 		greeting.greet("");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown");
+		greeting = null;
 	}
 
 }
