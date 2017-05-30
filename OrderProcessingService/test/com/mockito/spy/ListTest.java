b9fb634489d88053bef9c600e0277f0c3df8d2e8
Arvind <europa1613@users.noreply.github.com>
2017-05-29 20:09:03 -0400

    Spy vs Mock; Use when().thenCallRealMethod() if real call is needed

--- OrderProcessingService/test/com/mockito/spy/ListTest.java
+++ OrderProcessingService/test/com/mockito/spy/ListTest.java
@@ -1,6 +1,6 @@
 package com.mockito.spy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +8,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
@@ -16,7 +17,10 @@ public class ListTest {
 
 	@Spy
 	List<String> myList = new ArrayList<String>();
-	
+
+	@Mock
+	ArrayList<String> myListMock = new ArrayList<String>();
+
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
@@ -27,12 +31,27 @@ public class ListTest {
 	}
 
 	@Test
-	public void test() {
+	public void test_spy_doReturn() {
 		myList.add("Jupiter");
 		myList.add("Saturn");
-		//Mockito.when(myList.get(0)).thenReturn("Eclipse"); // comment above add's, cannot use with spy, IndexOutOfBounds
 		Mockito.doReturn(3).when(myList).size();
 		assertEquals(3, myList.size());
+	}
+
+	@Test
+	public void test_spy_when() {
+		// Mockito.when(myList.get(0)).thenReturn("Eclipse"); // cannot use when
+		// with spy, IndexOutOfBounds -> calls actual method -- FAILS
+		Mockito.doReturn("Eclipse").when(myList).get(0);
+		assertEquals("Eclipse", myList.get(0));
+	}
+
+	@Test
+	public void test_mock() {
+		Mockito.when(myListMock.get(0)).thenReturn("Eclipse");
+		Mockito.when(myListMock.size()).thenCallRealMethod();
+		assertEquals(0, myListMock.size());
+		assertEquals("Eclipse", myListMock.get(0));
 	}
 
 }
